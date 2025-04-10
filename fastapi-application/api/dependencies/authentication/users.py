c5aa899851cba95fc871f75d917acd99cf575bc9
Olexandr Tereshchenko <Terya77812@gmail.com>
2025-04-11 00:36:31 +0300

    create_superuser programmatic

--- fastapi-application/api/dependencies/authentication/users.py
+++ fastapi-application/api/dependencies/authentication/users.py
@@ -1,6 +1,5 @@
 from typing import TYPE_CHECKING, Annotated
 from fastapi import Depends
-from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from core.models.db_helper import db_helper
 from core.models.user import User
 if TYPE_CHECKING:

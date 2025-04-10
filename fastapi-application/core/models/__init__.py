dbdc6c08c6f55b7ba0a3099a7cd36b783365a821
Olexandr Tereshchenko <Terya77812@gmail.com>
2025-04-10 15:08:29 +0300

    change import for SQLAlchemyBaseUserTable

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -7,3 +7,4 @@ __all__ = (
 from .db_helper import db_helper
 from .base import Base
 from .user import User
+from .access_token import AccessToken
--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,5 +1,5 @@
 from typing import TYPE_CHECKING
-from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from core.models.base import Base
 from core.models.mixins.id_int_pk import IdIntPkMixin
 from core.types.user_id import UserIDType

ee0df410c9c616717aa9a6f78b44c1488a2df2d7
Olexandr Tereshchenko <Terya77812@gmail.com>
2025-04-10 23:15:50 +0300

    create users router

--- fastapi-application/api/api_v1/__init__.py
+++ fastapi-application/api/api_v1/__init__.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 from .auth import router as auth_router
+from .users import router as users_router
 from core.config import settings
+
+
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
 router.include_router(auth_router)
+router.include_router(users_router)
